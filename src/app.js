13e9d55bc5b61fa09439515d64b594c6981dbf32
Maxi <emanuel-salas@hotmail.com>
2025-05-13 20:21:11 -0300

    correcion

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
-
+import dotenv from 'dotenv';
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8080;
-const connection = mongoose.connect(`URL DE MONGO`)
+try {
+
+    const connection = mongoose.connect(process.env.MONGO_URL)
+    connection && console.log("BASE DE DATOS CONECTADA");
+} catch (error) {
+    console.log(error)
+}
+
 
 app.use(express.json());
 app.use(cookieParser());
