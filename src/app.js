e4b24a2c8ba4915d3457e55a1996f896fb0c91ff
Maxi <emanuel-salas@hotmail.com>
2025-05-13 20:39:27 -0300

    pruebamongop

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 try {
-    console.log(process.env.MONGO_URL)
+    console.log("MONGO_URL en runtime:", process.env.MONGO_URL);
     console.log(typeof process.env.MONGO_URL)
     const connection = mongoose.connect(process.env.MONGO_URL)
     connection && console.log("BASE DE DATOS CONECTADA");
